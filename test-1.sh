ea55ad37399861dd300f847c7606f35c42d01c3e
liamplm <liamplm82@gmail.com>
2020-04-14 00:58:18 +0430

    fix test titles

--- test-1.sh
+++ test-1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf "\nTest 3:\n"
+printf "\nTest 1:\n"
 
 time (python3 main-cache.py ./test-1.txt > /dev/null)
 time (python3 1.py ./test-1.txt > /dev/null)
--- test-2.sh
+++ test-2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf "\nTest 3:\n"
+printf "\nTest 2:\n"
 
 time (python3 main-cache.py ./test-2.txt > /dev/null)
 time (python3 1.py ./test-2.txt > /dev/null)
