fa5df6005bb80135b5ec060fa8f010c810c31024
liamplm <liamplm82@gmail.com>
2020-04-15 09:34:22 +0430

    remove unnecessary files; using fgets instead of fscanf

--- prime/main-xxx.c
+++ prime/main-xxx.c
@@ -5,11 +5,14 @@ int main(int argc, char *argv[])
 {
 
     FILE *file = fopen(argv[1], "r");
-    int i = 0, n = 0;
+    char buf[1000];
 
-    while (!feof(file))
+    int n = 0;
+
+    while (fgets(buf, 1000, file) != NULL)
     {
-        fscanf(file, "%d", &i);
+        // long long int num = atoi(buf);
+
         if (n == 5) {
             printf("1\n");
         } else {
