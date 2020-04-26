4e0df4d83ab12c4b7e120be2159b7e1ed3852732
liamplm <liamplm82@gmail.com>
2020-04-26 23:17:37 +0430

    just a test implementation of countme

---
+++ countme/main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express()
+
+let count = 0;
+
+app.post('/', (req, res) => {
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    })
+    req.on('end', () => {
+        //console.log('end => ', body)
+        count += parseInt(body);
+
+        res.sendStatus(200);
+    })
+
+})
+
+app.get('/count', (req, res) => {
+    //console.log('==>', count)
+    res.send(count.toString())
+})
+
+const PORT = 8085;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`server is listening on ${PORT} ...`)
+})
+
+
